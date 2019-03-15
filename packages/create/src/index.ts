da48947ba29817dad8439cfab13161dab4a23ab1
Philip Balo <f.g.balo@gmail.com>
2019-03-15 18:40:00 +0200

    Add script for amplify cli

--- packages/create/src/index.ts
+++ packages/create/src/index.ts
@@ -1,4 +1,6 @@
 import generate, { templateWriter } from '@larix/generator';
+import { spawn } from 'child_process';
+import chalk from 'chalk';
 import 'source-map-support/register';
 
 import templates from './templates';
@@ -12,4 +14,36 @@ process
     process.exit(1);
   });
 
-generate(templates, templateWriter, 'yarn create @amplify-app', process.argv);
+(async () => {
+  const { appName } = await generate(templates, templateWriter, 'yarn create @amplify-app', process.argv);
+
+  process.chdir(`./${appName}`);
+  
+  const AMPLIFY = "{\
+    \"projectName\":\"headlessProjectName\",\
+    \"envName\":\"myenvname\",\
+    \"defaultEditor\":\"code\"\
+    }"
+
+  const REACTCONFIG="{\
+    \"SourceDir\":\"src\",\
+    \"DistributionDir\":\"build\",\
+    \"BuildCommand\":\"npm run-script build\",\
+    \"StartCommand\":\"npm run-script start\"\
+    }"
+
+  const FRONTEND=`{\
+    \"frontend\":\"javascript\",\
+    \"framework\":\"react\",\
+    \"config\": ${REACTCONFIG}
+    }`
+  await new Promise(resolve => {
+    const amplify = spawn('amplify', ['init', '--amplify', AMPLIFY, '--frontend', FRONTEND], { stdio: 'inherit' });
+    amplify.on('close', resolve);
+  });
+
+  console.log(`App ${chalk.green(appName)} generated successfully! Execute commands below to start it:\n`);
+  console.log(chalk.yellow(`cd ${appName}`));
+  console.log(chalk.yellow(`yarn start`));
+  
+})();
